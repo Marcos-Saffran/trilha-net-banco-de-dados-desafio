11d2872b71742d85f35698da7c8a9267bb8e812f
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-05-28 09:37:36 -0300

    feat: resolução do sexto desafio

--- Script Resolução do desafio.sql
+++ Script Resolução do desafio.sql
@@ -45,4 +45,18 @@ FROM
 	Filmes
 WHERE
 	Ano > 2000;
+
+-- 6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM
+	Filmes
+WHERE
+	Duracao > 100
+	AND
+	Duracao < 150
+ORDER BY
+	Duracao;
 	
