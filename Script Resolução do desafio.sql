390634c71dd7c76916ab73e78c72c569e0856f96
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-05-28 10:08:30 -0300

    feat: Resolução do desafio 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome

--- Script Resolução do desafio.sql
+++ Script Resolução do desafio.sql
@@ -80,6 +80,16 @@ FROM
 WHERE
 	UPPER(Genero) = 'M';
 
+-- 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT
+	PrimeiroNome,
+	UltimoNome
+FROM
+	Atores
+WHERE
+	UPPER(Genero) = 'F'
+ORDER BY
+	PrimeiroNome;
 
 
 	
