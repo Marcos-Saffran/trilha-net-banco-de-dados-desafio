09265912dd86c3b33a527e616cd84843f85ca99e
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-05-28 10:00:03 -0300

    feat: Resolução do desafio 7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente

--- Script Resolução do desafio.sql
+++ Script Resolução do desafio.sql
@@ -59,4 +59,18 @@ WHERE
 	Duracao < 150
 ORDER BY
 	Duracao;
+
+-- 7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
+SELECT
+	Ano,
+	COUNT(*) AS Quantidade
+FROM 
+	Filmes
+GROUP BY
+	Ano
+ORDER BY
+	SUM(Duracao) DESC;
+
+
+
 	
